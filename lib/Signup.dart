521b1de5425523770833ff4436b38fe1e536feab
lailicahyani <202410102013@mail.unej.ac.id>
2022-06-06 08:43:49 +0700

    masih error

--- lib/Signup.dart
+++ lib/Signup.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:pbma11/home.dart';
+import 'package:pbma11/Login.dart';
 
-class Signup extends StatelessWidget {
-  const Signup({Key? key}) : super(key: key);
+class Signup extends StatefulWidget {
+  const Signup({ Key? key }) : super(key: key);
 
   @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  Widget _backButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 0, top: 10, bottom: 10),
+              child: Icon(Icons.keyboard_arrow_left, color: Colors.black),
+            ),
+            Text('Back',
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  TextEditingController dateinput = TextEditingController();
+
+  void initState() {
+    dateinput.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -22,16 +55,20 @@ class Signup extends StatelessWidget {
                           BorderRadius.only(topLeft: Radius.circular(35))),
                   child: Column(
                     children: [
+                      const SizedBox(height: 10),
                       const Padding(
-                        padding: EdgeInsets.fromLTRB(10, 20, 300, 10),
-                        child: Text(
-                          "Buat Akun Baru",
-                          style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                        padding: EdgeInsets.only(right: 250),
+                        // child : Center(
+                          child: Text(
+                            "Buat Akun Baru",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
                           ),
-                        ),
+                        // ),
                       ),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(30, 10, 30, 15),
@@ -78,7 +115,7 @@ class Signup extends StatelessWidget {
                             onPressed: () {
                               Navigator.push(context,
                                   MaterialPageRoute(builder: (context) {
-                                return const HomeWidget();
+                                return const HomePageWidget();
                               }));
                             },
                             child: const Text(
@@ -99,3 +136,4 @@ class Signup extends StatelessWidget {
     );
   }
 }
+
